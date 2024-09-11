c22e3734a4bbbf62a345e63a59e9232207bc9558
tubi55 <tubi55@nate.com>
2024-09-11 11:25:38 +0900

    문서객체에 접근해서 스타일,속성,텍스트,태그구조, 이벤트 연결하는법

--- script.js
+++ script.js
@@ -1,42 +1,28 @@
-//단수의 html요소 선택시
 const h1 = document.querySelector("h1");
-console.log(h1);
-//console.dir(h1);
-
-//특정 부모요소 안쪽의 자식 요소 탐색 선택
-const title = document.querySelector(".title a");
-console.log(title);
-
-//복수개 요소 탐색하는 방법
+const link = document.querySelector(".title a");
 const lis = document.querySelectorAll("ul li");
 
-//배열형태로 반환된 복수개의 html요소 하나씩 선택
-for (let i = 0; i < 4; i++) {
-  console.log(lis[i]);
-}
+console.dir(h1);
+console.dir(link);
+console.dir(lis[0]);
 
-//화살표함수 표현식과 forEach내장함수를 이용한 DOM반복 선택방법
-//배열.forEach((반복요소,순번)=>{반복코드})
-lis.forEach((el) => console.log(el));
+//DOM 스타일 제어 (style)
+h1.style.backgroundColor = "pink";
 
-//자바스크립트에서 함수 만드는 법
-//1. 함수선언식
-function plus(num1, num2) {
-  const result = num1 + num2;
-  return result;
-}
-console.log(plus(2, 4));
+//DOM 속성 변경 (attribute)
+link.href = "https://www.naver.com";
 
-//2. 함수표현식 (변수에 익명함수 대입)
-const minus = function (num1, num2) {
-  const result = num1 - num2;
-  return result;
+//DOM의 텍스트 변경 (textNode)
+lis[0].innerText = "목록1";
+
+//DOM의 html구성 변경 (elementNode)
+lis[1].innerHTML = "<span>list2</span>";
+
+//이벤트 : 사용자, 시스템이 웹상에서 발생시키는 모든 액션
+// 사용자 이벤트 : click, mouseover, mouseout, resize, scroll, mousewheel, mousemove
+// 시스템 이벤트 : load, error,
+
+//DOM의 이벤트 연결
+lis[2].onclick = () => {
+  console.log("You clicked list3");
 };
-console.log(minus(3, 1));
-
-//3. 화살표 함수로 축약된 함수 표현식
-//화살표 함수에서는 function키워드 생략, 중괄호 안쪽에 return문만 있으면 중괄호와 return문도 생략가능
-//화살표 함수는 단지 기존 코드의 축약 말고도
-//함수 내부의 this객체 생성을 막아서 고정해주는 효과 (객체지향 프로그램시 필요)
-const multiply = (num1, num2) => num1 * num2;
-console.log(multiply(3, 3));
